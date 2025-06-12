486f8b55fd3c01c1f521b501c04a7aabb7ab6327
mehul-raul <161235591+mehul-raul@users.noreply.github.com>
2025-06-12 12:29:41 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -222,6 +222,7 @@ with st.sidebar:
     selected = option_menu(
         menu_title="Choose Assessment",
         options=[
+            'Disease Prediction',
             'Diabetes Prediction',
             'Heart Disease Prediction',
             'Liver Prediction',
@@ -251,7 +252,35 @@ with st.sidebar:
         }
     )
 
+If selected == 'Disease Prediction': 
+    # Create disease class and load ML model
+    disease_model = DiseaseModel()
+    disease_model.load_xgboost('models/xgboost_model.json')
 
+    # Title
+    st.write('# Disease Prediction using Machine Learning')
+
+    symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
+
+    X = prepare_symptoms_array(symptoms)
+
+    # Trigger XGBoost model
+    if st.button('Predict'): 
+        # Run the model with the python script
+        
+        prediction, prob = disease_model.predict(X)
+        st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
+
+
+        tab1, tab2= st.tabs(["Description", "Precautions"])
+
+        with tab1:
+            st.write(disease_model.describe_predicted_disease())
+
+        with tab2:
+            precautions = disease_model.predicted_disease_precautions()
+            for i in range(4):
+                st.write(f'{i+1}. {precautions[i]}')
 
 # Sidebar menu with tiles
 # with st.sidebar:
