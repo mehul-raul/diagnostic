dfa6ddf08af00a608048124e25194b829b1f1573
mehul-raul <161235591+mehul-raul@users.noreply.github.com>
2025-06-20 09:35:12 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -4,14 +4,7 @@ st.set_page_config(
     page_icon="healthcare.png",  
 )
 
-st.markdown("""
-    <style>
-        /* Hide the top Streamlit navbar */
-        #MainMenu {visibility: hidden;}
-        header {visibility: hidden;}
-        footer {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
+
 
 import pandas as pd
 import joblib
