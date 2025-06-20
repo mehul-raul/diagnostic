242808dad57d12740cf3a84853caae2713e79002
mehul-raul <161235591+mehul-raul@users.noreply.github.com>
2025-06-20 09:32:28 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -3,6 +3,7 @@ st.set_page_config(
     page_title="TruDiagnosis",
     page_icon="healthcare.png",  
 )
+
 st.markdown("""
     <style>
         /* Hide the top Streamlit navbar */
@@ -11,42 +12,11 @@ st.markdown("""
         footer {visibility: hidden;}
     </style>
 """, unsafe_allow_html=True)
+
 import pandas as pd
 import joblib
 from PIL import Image
 from streamlit_option_menu import option_menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Choose Assessment",
-        options=[
-            'Diabetes Prediction',
-            'Heart Disease Prediction',
-            'Liver Prediction',
-            'Breast Cancer Prediction',
-            'Covid-19 Prediction',
-            'Lung Cancer Prediction'
-        ],
-        icons=['activity', 'heart-pulse', 'droplet', 'heart', 'virus', 'lungs'],
-        menu_icon="cast",
-        default_index=0,
-        orientation="vertical",
-        styles={
-            "container": {"padding": "5px", "background-color": "#581818"},
-            "icon": {"color": "#ffffff", "font-size": "20px"},
-            "nav-link": {
-                "font-size": "16px",
-                "text-align": "left",
-                "margin": "5px",
-                "color": "#ffffff",
-                "border-radius": "8px"
-            },
-            "nav-link-selected": {
-                "background-color": "#943131",
-                "font-weight": "bold",
-                "color": "#ffffff"
-            }
-        }
-    )
 
 # Load models
 diabetes_model = joblib.load("models/diabetes_model.sav")
@@ -255,42 +225,39 @@ st.markdown("""
 # ])
 
 
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Choose Assessment",
+        options=[
+            'Diabetes Prediction',
+            'Heart Disease Prediction',
+            'Liver Prediction',
+            'Breast Cancer Prediction',
+            'Covid-19 Prediction',
+            'Lung Cancer Prediction'
+        ],
+        icons=['activity', 'heart-pulse', 'droplet', 'heart', 'virus', 'lungs'],
+        menu_icon="cast",
+        default_index=0,
+        orientation="vertical",
+        styles={
+            "container": {"padding": "5px", "background-color": "#581818"},
+            "icon": {"color": "#ffffff", "font-size": "20px"},
+            "nav-link": {
+                "font-size": "16px",
+                "text-align": "left",
+                "margin": "5px",
+                "color": "#ffffff",
+                "border-radius": "8px"
+            },
+            "nav-link-selected": {
+                "background-color": "#943131",
+                "font-weight": "bold",
+                "color": "#ffffff"
+            }
+        }
+    )
 
-
-
-# Sidebar menu with tiles
-# with st.sidebar:
-#     selected = option_menu(
-#         menu_title="Choose Assessment",
-#         options=[
-#             'Diabetes Prediction',
-#             'Heart Disease Prediction',
-#             'Liver Prediction',
-#             'Breast Cancer Prediction',
-#             'Covid-19 Prediction',
-#             'Lung Cancer Prediction'
-#         ],
-#         icons=['activity', 'heart-pulse', 'droplet', 'female', 'virus', 'lungs'],
-#         menu_icon="cast",
-#         default_index=0,
-#         orientation="vertical",
-#         styles={
-#             "container": {"padding": "5px", "background-color": "#003366"},
-#             "icon": {"color": "#ffffff", "font-size": "20px"},
-#             "nav-link": {
-#                 "font-size": "16px",
-#                 "text-align": "left",
-#                 "margin": "5px",
-#                 "color": "#ffffff",
-#                 "border-radius": "8px"
-#             },
-#             "nav-link-selected": {
-#                 "background-color": "#005eb8",
-#                 "font-weight": "bold",
-#                 "color": "#ffffff"
-#             }
-#         }
-#     )
 
 
 if selected == 'General Disease Prediction': 
